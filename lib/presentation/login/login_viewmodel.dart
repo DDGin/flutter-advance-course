da593eabc9a8137f82d6a875f31860860f57864e
Dao Tien Dung <dung226dtd@gmail.com>
2025-02-01 16:34:12 +0700

    Data Classes

--- lib/presentation/login/login_viewmodel.dart
+++ lib/presentation/login/login_viewmodel.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter_advance_course/presentation/base/baseviewmodel.dart';
+import 'package:flutter_advance_course/presentation/common/freezed_data_classes.dart';
 
 class LoginViewModel
     implements BaseViewModel, LoginViewModelInputs, LoginViewModelOutputs {
@@ -8,6 +9,8 @@ class LoginViewModel
       StreamController<String>.broadcast();
   StreamController _passwordStreamController =
       StreamController<String>.broadcast();
+
+  var loginObject = LoginObject("", "");
 
   // inputs
   @override
@@ -38,11 +41,15 @@ class LoginViewModel
   @override
   setPassword(String password) {
     inputPassword.add(password);
+    loginObject = loginObject.copyWith(
+        password: password); // data class operation same as kotlin
   }
 
   @override
   setUserName(String userName) {
     inputUserName.add(userName);
+    loginObject = loginObject.copyWith(
+        userName: userName); // data class operation same as kotlin
   }
 
   // outputs
@@ -64,8 +71,6 @@ class LoginViewModel
   _isUserNameValid(String username) {
     return username.isNotEmpty;
   }
-
-  _printAbc() {}
 }
 
 abstract class LoginViewModelInputs {
