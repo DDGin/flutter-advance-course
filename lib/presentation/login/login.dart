4a4d326aec5582536799bde2f701c5d55c341a04
Dao Tien Dung <dung226dtd@gmail.com>
2025-02-09 18:21:35 +0700

    Login View UI Continue

--- lib/presentation/login/login.dart
+++ lib/presentation/login/login.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_advance_course/presentation/login/login_viewmodel.dart';
+import 'package:flutter_advance_course/presentation/resources/asset_manager.dart';
+import 'package:flutter_advance_course/presentation/resources/color_manager.dart';
+import 'package:flutter_advance_course/presentation/resources/string_manager.dart';
+import 'package:flutter_advance_course/presentation/resources/value_manager.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class LoginView extends StatefulWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -14,6 +19,8 @@ class _LoginViewState extends State<LoginView> {
 
   TextEditingController _userNameController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
+
+  final _formKey = GlobalKey<FormState>();
 
   _bind() {
     _viewModel.start();
@@ -38,5 +45,63 @@ class _LoginViewState extends State<LoginView> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
+  }
+
+  Widget _getContentWidget() {
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.only(top: AppPadding.p20),
+        color: ColorManager.white,
+        child: SingleChildScrollView(
+          child: Form(
+            child: Form(
+                child: Column(
+              children: [
+                SvgPicture.asset(ImageAssets.splashLogo),
+                SizedBox(height: AppSize.s28),
+                Padding(
+                  padding: EdgeInsets.only(
+                      left: AppPadding.p28, right: AppPadding.p28),
+                  child: StreamBuilder<bool>(
+                      stream: _viewModel.outputIsUserNameValid,
+                      builder: (context, snapshot) {
+                        return TextFormField(
+                          keyboardType: TextInputType.emailAddress,
+                          controller: _userNameController,
+                          decoration: InputDecoration(
+                              labelText: AppStrings.username,
+                              hintText: AppStrings.username,
+                              errorText: (snapshot.data ?? true)
+                                  ? null
+                                  : AppStrings.usernameError),
+                        );
+                      }),
+                ),
+                SizedBox(height: AppSize.s28),
+                Padding(
+                  padding: EdgeInsets.only(
+                      left: AppPadding.p28, right: AppPadding.p28),
+                  child: StreamBuilder<bool>(
+                      stream: _viewModel.outputIsPasswordValid,
+                      builder: (context, snapshot) {
+                        return TextFormField(
+                          keyboardType: TextInputType.visiblePassword,
+                          controller: _passwordController,
+                          decoration: InputDecoration(
+                              labelText: AppStrings.password,
+                              hintText: AppStrings.password,
+                              errorText: (snapshot.data ?? true)
+                                  ? null
+                                  : AppStrings.passwordError),
+                        );
+                      }),
+                ),
+              ],
+            )),
+            key: _formKey,
+          ),
+        ),
+      ),
+    );
   }
 }
